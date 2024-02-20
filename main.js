5f763a31ad50719c6b3bb75d8f84525c208e27c5
YEBEIN YOON <younyb33@naver.com>
2024-02-20 22:01:32 +0900

    second commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const API_KEY = `cf14a883ebcc43a8a235a2ba6ae0e1c7`
 let news = []
  const getNews = async()=>{
     const url = new URL(
-        `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`
+        `https://times-yeriel.netlify.app/`
         );
     const response =  await fetch(url);
     const data = await response.json(); // json은 파일 형식 중 하나이다. 파일의 확장자이다. 객체를 텍스트화한 타입이다.
